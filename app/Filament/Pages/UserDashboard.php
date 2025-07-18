45e253cc4954e06032ac885c872da64f8461be8e
Azhari <muhd.azhari.ayie@gmail.com>
2025-07-18 11:16:29 +0800

    User Dashboard implemented with role-based access

---
+++ app/Filament/Pages/UserDashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class UserDashboard extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+    protected static ?string $title = 'User Dashboard';
+    protected static string $view = 'filament.pages.user-dashboard';
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()?->hasAnyRole(['user', 'manager', 'vendor']);
+    }
+}
